658e8113cf64abfa74779a9da21acd54ef917ec1
Sergio Mavie <sergio.mavie@gmail.com>
2025-01-25 11:28:53 +0200

    Added code for getting the book details based on ISBN

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "isbn": "ABC-123", "reviews": {} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "isbn": "DEF-456", "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "isbn": "GHI-489", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "isbn": "IJK-012", "reviews": {} },
+      5: {"author": "Unknown","title": "The Book Of Job", "isbn": "LMN-345", "reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "isbn": "OPQ-678", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "isbn": "RST-901", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "isbn": "UVW-234", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "isbn": "XYZ-567", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn": "XXY-999", "reviews": {} }
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,10 +17,19 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+
+public_users.get("/books/:isbn", (req, res) => {
+  const { isbn } = req.params;
+
+  // Find the book with the given ISBN
+  const book = Object.values(books).find((b) => b.isbn === isbn);
+
+  if (book) {
+    res.status(200).json({ message: "Book found", book });
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
