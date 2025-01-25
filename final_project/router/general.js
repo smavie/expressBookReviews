2757c447bf0764f658692e2eef5b29fbe0dd33bc
Sergio Mavie <sergio.mavie@gmail.com>
2025-01-25 13:04:00 +0200

    Added code for registering a new use

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -73,4 +73,37 @@ public_users.get('/review/:isbn', function (req, res) {
   }
 });
 
+// Register a new user
+public_users.post("/user/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if both username and password are provided
+  if (username && password) {
+      // Check if the user does not already exist
+      if (!doesExist(username)) {
+          // Add the new user to the users array
+          users.push({"username": username, "password": password});
+          return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+          return res.status(404).json({message: "User already exists!"});
+      }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+const doesExist = (username) => {
+  // Filter the users array for any user with the same username
+  let userswithsamename = users.filter((user) => {
+      return user.username === username;
+  });
+  // Return true if any user with the same username is found, otherwise false
+  if (userswithsamename.length > 0) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
 module.exports.general = public_users;
