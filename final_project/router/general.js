bc955d8f67496e8736782d806b8572adf9b6d421
Sergio Mavie <sergio.mavie@gmail.com>
2025-01-25 12:22:19 +0200

    Added code for getting the book details based on the author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,10 +31,18 @@ public_users.get("/books/:isbn", (req, res) => {
   }
 });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Endpoint to get books by author
+public_users.get('/author/:author', function (req, res) {
+  const { author } = req.params;
+
+  // Get all the books and filter by author
+  const matchingBooks = Object.values(books).filter((book) => book.author.toLowerCase() === author.toLowerCase());
+
+  if (matchingBooks.length > 0) {
+    res.status(200).json({ message: "Books found", books: matchingBooks });
+  } else {
+    res.status(404).json({ message: `No books found by author: ${author}` });
+  }
 });
 
 // Get all books based on title
